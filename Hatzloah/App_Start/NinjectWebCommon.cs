9ac746f10c38b8deae61a15d2f077359b4fe69c0
Steve Goldman <gutezach@gmail.com>
2018-02-21 12:27:58 -0500

    Added Ninject File

---
+++ Hatzloah/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,68 @@
+﻿using Hatzloah.App_Start;
+using Hatzloah.Controllers;
+using Hatzloah.DAL;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Configuration;
+using System.Data.Entity;
+using System.Web;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+namespace Hatzloah.App_Start
+{
+    public class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            Database.SetInitializer(new NullDatabaseInitializer<HatzolahOrm>());
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            kernel.Bind<HatzolahOrm>().ToSelf().WhenInjectedInto<HomeController>().InSingletonScope().WithConstructorArgument("connectionString", connectionString);
+        }
+    }
+}
