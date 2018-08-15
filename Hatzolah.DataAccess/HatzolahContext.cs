f759cab707dbf5adacb28ac420a6d9a30c127911
Steve Goldman <gutezach@gmail.com>
2018-08-15 09:03:23 -0400

    Unignored context files

---
+++ Hatzolah.DataAccess/HatzolahContext.cs
@@ -0,0 +1,82 @@
+﻿using Hatzloah.DataAccess;
+using Hatzloah.DataAccess.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hatzolah.DataAccess
+{
+    public class HatzolahContext : IHatzolahContext
+    {
+        private string connectionString;
+
+        public HatzolahContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Dictionary<int, string> GetAllNocs()
+        {
+            using (HatzolahOrm orm = new HatzolahOrm(connectionString))
+            {
+                var nocList = orm.NocCodes.ToDictionary(x => x.Id, x => x.Description + " NOC: " + x.Noc);
+
+                return nocList;
+            }
+        }
+
+        public HatzolahNoc GetDefaultNoc(string description)
+        {
+            HatzolahNoc noc = new HatzolahNoc
+            {
+                Tone = "Single Tone or use your own judgement",
+                Instructions = "Dispatch Two Units  or use your own judgement",
+                Noc = description
+            };
+
+            return noc;
+        }
+
+        public HatzolahNoc GetNoc(int id)
+        {
+            var orm = new HatzolahOrm(connectionString);
+            var noc = orm.NocCodes.Where(x => x.Id == id).FirstOrDefault();
+
+            if (noc != null)
+            {
+                var nocMapped = MapNoc(noc);
+
+                if (noc.QuestionId != null)
+                {
+                    GetNocQuestion(orm, nocMapped);
+                }
+
+                return nocMapped;
+            }
+
+            return noc;
+        }
+
+        private void GetNocQuestion(HatzolahOrm orm, HatzolahNoc noc)
+        {
+            var nocQuestionResult = orm.Question.Select(x => new { x.Question, x.QuestionId }).Where(m => m.QuestionId == noc.QuestionId).FirstOrDefault();
+            if (nocQuestionResult != null)
+            {
+                noc.Question = nocQuestionResult.Question;
+
+            }
+        }
+
+        private HatzolahNoc MapNoc(HatzolahNoc noc)
+        {
+            var hatzolahNoc = new HatzolahNoc();
+            hatzolahNoc.Tone = noc.Tone;
+            hatzolahNoc.CodeOneFlag = noc.CodeOneFlag;
+            hatzolahNoc.Instructions = noc.Instructions;
+            hatzolahNoc.Noc = noc.Noc;
+
+            return noc;
+        }
+    }
+}
---
+++ Hatzolah.DataAccess/IHatzolahContext.cs
@@ -0,0 +1,15 @@
+﻿using Hatzloah.DataAccess;
+using System;
+using System.Collections.Generic;
+
+namespace Hatzolah.DataAccess
+{
+    public interface IHatzolahContext
+    {
+        Dictionary<int, string> GetAllNocs();
+
+        HatzolahNoc GetNoc(int id);
+
+        HatzolahNoc GetDefaultNoc(string description);
+    }
+}
